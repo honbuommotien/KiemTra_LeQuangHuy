9f1cb9c93055c89d4200251de349782f955587de
Le Quang Huy <qu4nghuyle@gmail.com>
2022-06-13 19:37:44 +0700

    ...

---
+++ KiemTra_LeQuangHuy/Controllers/SinhVienController.cs
@@ -0,0 +1,110 @@
+﻿using KiemTra_LeQuangHuy.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KiemTra_LeQuangHuy.Controllers
+{
+    public class SinhVienController : Controller
+    {
+        MyDataDataContext data = new MyDataDataContext();
+        public ActionResult Index()
+        {
+            var all_sinhvien = from ss in data.SinhViens select ss;
+            return View(all_sinhvien);
+        }
+        public ActionResult Detail(string id)
+        {
+            var D_sinhvien = data.SinhViens.Where(m => m.MaSV == id).First();
+            return View(D_sinhvien);
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(FormCollection collection, SinhVien s)
+        {
+            var E_MaSV = collection["MaSV"];
+            var E_HoTen = collection["HoTen"];
+            var E_GioiTinh = collection["GioiTinh"];
+            var E_NgaySinh = Convert.ToDateTime(collection["NgaySinh"]);
+            var E_Hinh = collection["Hinh"];
+            var E_MaNganh = collection["MaNganh"];
+
+            if (string.IsNullOrEmpty(E_HoTen))
+            {
+                ViewData["Error"] = "Don't empty!";
+            }
+            else
+            {
+                s.MaSV = E_MaSV.ToString();
+                s.HoTen = E_HoTen.ToString();
+                s.GioiTinh = E_GioiTinh.ToString();
+                s.NgaySinh = E_NgaySinh;
+                s.Hinh = E_Hinh.ToString();
+                s.MaNganh = E_MaNganh.ToString();
+                data.SinhViens.InsertOnSubmit(s);
+                data.SubmitChanges();
+                return RedirectToAction("Index");
+            }
+            return this.Create();
+        }
+        public ActionResult Edit(string id)
+        {
+            var E_SinhVien = data.SinhViens.First(m => m.MaSV == id);
+            return View(E_SinhVien);
+        }
+        [HttpPost]
+        public ActionResult Edit(string id, FormCollection collection)
+        {
+            var E_SinhVien = data.SinhViens.First(m => m.MaSV == id);
+            var E_Hoten = collection["Hoten"];
+            var E_GioiTinh = collection["GioiTinh"];
+            var E_NgaySinh = Convert.ToDateTime(collection["NgaySinh"]);
+            var E_Hinh = collection["Hinh"];
+            var E_MaNganh = collection["MaNganh"];
+            E_SinhVien.MaSV = id;
+            if (string.IsNullOrEmpty(E_Hoten))
+            {
+                ViewData["Error"] = "Don't empty!";
+            }
+            else
+            {
+                E_SinhVien.HoTen = E_Hoten;
+                E_SinhVien.GioiTinh = E_GioiTinh;
+                E_SinhVien.NgaySinh = E_NgaySinh;
+                E_SinhVien.Hinh = E_Hinh;
+                E_SinhVien.MaNganh = E_MaNganh;
+                UpdateModel(E_SinhVien);
+                data.SubmitChanges();
+                return RedirectToAction("Index");
+            }
+            return this.Edit(id);
+        }
+        public ActionResult Delete(string id)
+        {
+            var D_sach = data.SinhViens.First(m => m.MaSV == id);
+            return View(D_sach);
+        }
+        [HttpPost]
+        public ActionResult Delete(string id, FormCollection collection)
+        {
+            var D_sach = data.SinhViens.Where(m => m.MaSV == id).First();
+            data.SinhViens.DeleteOnSubmit(D_sach);
+            data.SubmitChanges();
+            return RedirectToAction("Index");
+        }
+        public string ProcessUpload(HttpPostedFileBase file)
+        {
+            if (file == null)
+            {
+                return "";
+            }
+            file.SaveAs(Server.MapPath("~/Content/images/" + file.FileName));
+            return "/Content/images/" + file.FileName;
+        }
+    }
+}
